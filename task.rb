dfeb82fe9812beac8898efbcb8dcce0594e1a6a4
Genki Nishiyama <genki.nishiyama@gmail.com>
2020-04-26 18:07:30 +0900

    質問6,7,8を修正

--- task.rb
+++ task.rb
@@ -48,14 +48,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  p numbers1.map{ |n| n * 10 }
+  p numbers2 = numbers1.map{ |n| n * 10 }
+  
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array = array.map(&:to_i)
+  array = array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -64,8 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map{ |str| str.capitalize }
-  upper_case_programming_languages = programming_languages.map{ |str| str.upcase }
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
